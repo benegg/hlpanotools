8414163c7226dba5287bb65132a8545a5a20223f
casa <cuijiemmx@163.com>
2013-05-15 16:51:36 +0800

    mktour.py doesn't have to be execute in the host directory now

--- mktour.py
+++ mktour.py
@@ -16,7 +16,8 @@ def _mktour(src, dest):
 	for dirpath, dirnames, filenames in os.walk(src):
 		[shutil.rmtree(os.path.join(dirpath, d)) for d in dirnames]
 		paths = ['"%s"' % os.path.join(dirpath, filename) for filename in filenames]	
-		cmd = './krpanotools-1.16.1-mac64/kmakemultires ./krpanotools-1.16.1-mac64/templates/vtour-normal.config %s' % ' '.join(paths)
+		scriptpath = sys.path[0]
+		cmd = '%s/krpanotools-1.16.1-mac64/kmakemultires %s/krpanotools-1.16.1-mac64/templates/vtour-normal.config %s' % (scriptpath, scriptpath, ' '.join(paths))
 		print(cmd)
 		os.system(cmd)
 	normdir = os.path.normpath(dirpath)
@@ -28,6 +29,7 @@ def _create_tmp(src, tmp):
 	for dirpath, dirnames, filenames in os.walk(src):
 		for filename in filenames:
 			basename, ext  = os.path.splitext(filename)
+			destname = filename
 			if len(basename) > 4:
 				num, b64name = basename[0:4], basename[4:]
 				try:
@@ -35,14 +37,13 @@ def _create_tmp(src, tmp):
 					destname = num + destname.replace(os.path.sep, '-') + ext
 				except TypeError:
 					pass
-			else:
-				destname = filename
 			linkfrom = os.path.join(src, filename)
 			linkto = os.path.join(tmp, destname)
 			print '%s -> %s' % (linkfrom, linkto)
 			os.link(linkfrom, linkto)
 
 def main():
+	print sys.path[0] 
 	parser = argparse.ArgumentParser(description='make pano tour')
 	parser.add_argument('-p', '--preview', action='store_true', help='open tour after making')
 	parser.add_argument('dir', type=_isdir, help='panorama directory')
