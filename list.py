ddd2735104028090d53c47805d24b69967b482d0
casa <cuijiemmx@163.com>
2013-05-17 15:07:04 +0800

    sort output by number

--- list.py
+++ list.py
@@ -4,7 +4,6 @@ import os
 import base64
 import argparse
 
-
 def _dir(path):
 	if not os.path.isdir(path):
 		raise argparse.ArgumentTypeError('%s is not a directory' % path)
@@ -13,7 +12,7 @@ def _dir(path):
 
 def _list(dir):
 	for dirpath, dirnames, filenames in os.walk(dir):
-		for filename in filenames:
+		for filename in sorted(filenames):
 			if not filename.startswith('.'):
 				prefix = filename[0:3]
 				name, ext = os.path.splitext(filename[4:])
