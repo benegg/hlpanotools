12d16018cffa41728f5fff7016bf81481c4bec9e
casa <cuijiemmx@163.com>
2013-06-05 18:42:29 +0800

    default convert to 2048*2048 square

--- convert.py
+++ convert.py
@@ -13,14 +13,14 @@ def get_scene_href(root, linkedscene):
 		if name.lower() == linkedscene.lower():
 			return title
 	
-def convert(dir, outdir, larger):
+def convert(dir, outdir, smaller):
 	root = ET.parse(os.path.join(dir, 'tour.xml'))
 	surfixes = ['r', 'l', 'u', 'd', 'f', 'b']
 
 	for scene in root.findall('scene'):
 		prefix = scene.attrib['title']
 		print('processing %s' % prefix)
-		cube = scene.find('image/cube').attrib['url'] if larger else scene.find('image/mobile/cube').attrib['url']
+		cube = scene.find('image/mobile/cube').attrib['url'] if smaller else scene.find('image/cube').attrib['url'] 
 		j = dict()
 
 		images = ['%s_%s.jpg' % (prefix, surfix) for surfix in surfixes]
@@ -57,13 +57,13 @@ def convert(dir, outdir, larger):
 def main():
 	parser = argparse.ArgumentParser(description='convert tour to dev format')	
 	parser.add_argument('tour', help='tour directory')
-	parser.add_argument('-x', '--larger', action='store_true', default=False, help='generate 2048x2048 images')
+	parser.add_argument('-s', '--smaller', action='store_true', default=False, help='generate smaller cube images')
 	args = parser.parse_args()	
 	ndir = os.path.normpath(args.tour)
-	outdir = "%s/%s" % ('dev', os.path.split(ndir)[1])
+	outdir = '%s.dev' % os.path.splitext(ndir)[0]
 	shutil.rmtree(outdir, ignore_errors=True)
 	os.makedirs(outdir)
-	convert(ndir, outdir, args.larger)
+	convert(ndir, outdir, args.smaller)
 
 if __name__ == '__main__':
 	main()
