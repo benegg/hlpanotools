91c8d2d07b79d03ce7eb5cc672017c52fa130808
casa <cuijiemmx@163.com>
2013-06-28 12:53:29 +0800

    modify for general project

--- hsmap.py
+++ hsmap.py
@@ -8,47 +8,31 @@ import json
 import itertools
 import codecs
 
-aeras = ['Chu xi', 'Gao bei', 'Hong keng', 'Nan xi']
-
 def _dir(path):
 	if not os.path.isdir(path):
 		raise argparse.ArgumentTypeError('%s is not a directory' % path)
 	else:
 		return path
 
-def main():
-	parser = argparse.ArgumentParser(description='generate hotspot mapping')
-	parser.add_argument('-a', '--add', action='store_true', help='add image info from hotspot mapping')
-	parser.add_argument('dir', type = _dir, help='root Tour3D directory contains hotspot info')
-	args = parser.parse_args()
-
-	dir = args.dir
-	xls = 'hsmap.xls'
-
-	if args.add:
-		apply_mapping(dir, xls)
-	else:
-		gen_mapping(dir, xls)
 
 def gen_mapping(dir, xls):
 	wb = xlwt.Workbook(encoding='utf8')
-	for aera in aeras:
-		st = wb.add_sheet(aera)
-		st.write(0, 0, 'from')
-		st.write(0, 1, 'to')
-		st.write(0, 2, 'image')
-		index = 1
-		for dirpath, dirnames, filenames in os.walk(os.path.join(dir, aera)):
-			for filename in filenames:
-				if filename.endswith('.json'):
-					with open(os.path.join(dirpath, filename), 'r') as fp:
-						content = json.load(fp, encoding='utf8')
-						fname = os.path.splitext(filename)[0]
-						for hs in content['hotspots']:
-							tname = os.path.splitext(hs['href'])[0]
-							st.write(index, 0, fname)
-							st.write(index, 1, tname)
-							index += 1
+	st = wb.add_sheet('mapping')
+	st.write(0, 0, 'from')
+	st.write(0, 1, 'to')
+	st.write(0, 2, 'image')
+	index = 1
+	for dirpath, dirnames, filenames in os.walk(dir):
+		for filename in filenames:
+			if filename.endswith('.json'):
+				with open(os.path.join(dirpath, filename), 'r') as fp:
+					content = json.load(fp, encoding='utf8')
+					fname = os.path.splitext(filename)[0]
+					for hs in content['hotspots']:
+						tname = os.path.splitext(hs['href'])[0]
+						st.write(index, 0, fname)
+						st.write(index, 1, tname)
+						index += 1
 	wb.save(xls)
 
 def find_image(image_list, tname):
@@ -57,17 +41,16 @@ def find_image(image_list, tname):
 def apply_mapping(dir, xls):
 	wb = xlrd.open_workbook(xls)
 	hsdict = {}
-	for aera in aeras:
-		st = wb.sheet_by_name(aera)
-		for i in xrange(1, st.nrows):
-			fname = st.cell_value(i, 0)
-			tname = st.cell_value(i, 1)
-			image = st.cell_value(i, 2)
-			jsonfile = os.path.join(dir, aera, fname + '.json')
-			if not hsdict.has_key(jsonfile):
-				hsdict[jsonfile] = [(tname, image)]
-			else:
-				hsdict[jsonfile].append((tname, image))	
+	st = wb.sheet_by_index(0)
+	for i in xrange(1, st.nrows):
+		fname = st.cell_value(i, 0)
+		tname = st.cell_value(i, 1)
+		image = st.cell_value(i, 2)
+		jsonfile = os.path.join(dir, fname + '.json')
+		if not hsdict.has_key(jsonfile):
+			hsdict[jsonfile] = [(tname, image)]
+		else:
+			hsdict[jsonfile].append((tname, image))	
 	for item in hsdict.items():
 		jsonfile = item[0]
 		with open(jsonfile, 'r') as fp:	
@@ -83,6 +66,20 @@ def apply_mapping(dir, xls):
 		with codecs.open(jsonfile, 'w', encoding='utf8') as fp:
 			value = json.dumps(content, indent=4, separators=(',', ': '))
 			fp.write(value.decode('unicode-escape'))
+
+def main():
+	parser = argparse.ArgumentParser(description='generate hotspot mapping')
+	parser.add_argument('-a', '--add', action='store_true', help='add image info from hotspot mapping')
+	parser.add_argument('dir', type = _dir, help='data directory')
+	args = parser.parse_args()
+
+	dir = args.dir
+	xls = 'hsmap.xls'
+
+	if args.add:
+		apply_mapping(dir, xls)
+	else:
+		gen_mapping(dir, xls)
 
 if __name__ == '__main__':
 	main()
